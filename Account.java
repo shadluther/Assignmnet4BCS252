80723bf351285a0a1db2728c37903f68b3b20cfb
shadluther <71608848+shadluther@users.noreply.github.com>
2020-10-20 10:12:01 -0500

    Add files via upload

---
+++ Account.java
@@ -0,0 +1,46 @@
+package employeeinfo.Employee;
+
+
+
+public class Account {
+	private final static double DEFAULT_BALANCE = 0.0;
+	private double balance;
+	private AccountType acctType;
+	private Employee employee;
+	
+	Account(Employee emp, AccountType acctType, double balance){
+		employee = emp;
+		this.acctType =acctType;
+		System.out.println(this.acctType.type);
+		this.balance = balance;
+	}
+	Account(Employee emp, AccountType acctType){
+		this(emp,acctType,DEFAULT_BALANCE);	
+		
+		
+	}
+	public double getBalance() {
+		return balance;
+	}
+	
+	public AccountType getAcctType() {
+		return acctType;
+	}
+	public String toString() {
+		String newline = System.getProperty("line.separator");
+		return "Account type: "+ acctType.toString().toLowerCase()+
+				newline +
+				"Current bal: "+balance;
+	}
+
+	public void makeDeposit(double deposit) {
+		balance += deposit;
+	}
+	public boolean makeWithdrawal(double amount) {
+		if(amount <= balance) {
+			balance -= amount;
+			return true;
+		}
+		return false;
+	}
+}
